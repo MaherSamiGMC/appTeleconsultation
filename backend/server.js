b1fd11428691d71cdf7a55181885f9026671902b
Maher Sami <samimaher.ensit@gmail.com>
2021-06-16 23:05:16 +0100

    first commit

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express=require('express')
+const app=express()
+const cors = require('cors')
+require('dotenv').config()
+const connectDB= require('./config/connectDB')
+
+app.use(express.json())
+app.use(cors())
+
+connectDB()
+
+const PORT=process.env.PORT || 5000
+
+app.listen(PORT, console.log(`server running on port ${PORT}`) )
+
