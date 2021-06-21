a0b2269c59f05ceda9612982300576ccfc9288fa
Maher Sami <samimaher.ensit@gmail.com>
2021-06-21 12:11:10 +0100

    hashing error fixed & ommiting password rendering

--- backend/controllers/assistantControllers.js
+++ backend/controllers/assistantControllers.js
@@ -2,16 +2,18 @@ const asyncHandler = require('express-async-handler')
 const assistant=require('../models/AssistantSchema')
 const doctor=require('../models/DoctorSchema')
 const generateToken=require('../utils/generateToken')
+const bcrypt = require('bcryptjs')
+
 
 //Find assistants
 const getAssistants=asyncHandler(async(req,res)=>{
-    const assistants=await assistant.find().populate("affiliateDoctor")
+    const assistants=await assistant.find().populate("affiliateDoctor").select('-password')
     res.json({message:'assistants loaded successfully',assistants})
 })
 
 //Find assistant by id
 const getAssistant=asyncHandler(async(req,res)=>{
-    const getAssistant=await assistant.findById(req.params.id)
+    const getAssistant=await assistant.findById(req.params.id).select('-password')
     res.json({message:'Assistant data loaded successfully',getAssistant})
 })
 
@@ -31,13 +33,17 @@ const addNewAssistant=asyncHandler((async(req,res)=>{
 }))
 //update Assistant's data 
 const updateAssistant=asyncHandler((async(req,res)=>{
-    let updateAssistant=await assistant.findByIdAndUpdate(req.params.id, { $set: {...req.body}})
+    if(req.body.password){
+        const salt=await bcrypt.genSalt(10)
+        req.body.password= await bcrypt.hash(req.body.password,salt)
+    }
+    let updateAssistant=await assistant.findByIdAndUpdate(req.params.id, { $set: {...req.body}}).select('-password')
     res.json({message:'Assistant updated successfully',updateAssistant})
 }))
 
 //delete Assistant
 const deleteAssistant=asyncHandler((async(req,res)=>{
-    let deletedAssistant=await assistant.findByIdAndRemove(req.params.id)
+    let deletedAssistant=await assistant.findByIdAndRemove(req.params.id).select('-password')
     // the Assistant will be deleted from the object Assistant in doctor's document
     await doctor.findByIdAndUpdate(deletedAssistant.affiliateDoctor,{ $unset : {"assistant":deletedAssistant._id}})
     res.json({message:'Assistant deleted successfully',deletedAssistant})
