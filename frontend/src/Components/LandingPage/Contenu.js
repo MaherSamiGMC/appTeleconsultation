538c36d78782c4758ead8978a713cc7351305860
yosri abdelli <yosriabdelli1@gmail.com>
2021-06-22 23:25:50 +0100

    page d'accueil

---
+++ frontend/src/Components/LandingPage/Contenu.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Container, Col } from 'react-bootstrap'
+const Contenu = () => {
+    return (
+        <>
+            <Container>
+                <h1>Médecine connectée et téléconsultation</h1>
+                <Col>
+                    <p>Le secteur de la santé se développe de plus en plus notamment avec l’essor que connaît la télémédecine. Cette pratique médicale ouvre de nouvelles possibilités, offre
+                    des perspectives prometteuses et donne davantage accès aux soins.</p>
+                    <h2>La télémédecine : qu’est-ce que c’est ?</h2>
+                    <p>La télémédecine, ou médecine connectée est une composante de la télésanté. C'est une forme de pratique médicale qui regroupe les actes médicaux réalisés à distance
+                    moyennant les technologies de l’information et de la communication.</p>
+                    <p>La télémédecine est une nouvelle manière de soigner à part entière. Elle exige les mêmes bonnes pratiques de la médecine classique et impose la même rigueur en termes
+                    de qualité de soins, de sécurité des échanges, de prise en charge et de conseils.</p>
+                    <p>Cette pratique médicale fait évoluer la médecine classique et répond à de nombreux défis comme le vieillissement de la population, e suivi approfondi des maladies
+                    chroniques et les problèmes de mobilité.<br/>
+                    La télémédecine se divise en plusieurs pratiques : la téléconsultation, la téléexpertise, la télésurveillance et la téléassistance.</p>
+                </Col>
+            </Container>
+        </>
+    )
+}
+
+export default Contenu
--- frontend/src/Components/LandingPage/Header.js
+++ frontend/src/Components/LandingPage/Header.js
@@ -1,18 +1,20 @@
 import React from 'react'
-import { Navbar, Nav } from 'react-bootstrap'
+import { Navbar, Nav, Container } from 'react-bootstrap'
 const Header = () => {
     return (
         <header>
-            <Navbar bg="light" expand="lg">
-                <Navbar.Brand href="/">Near Doctor</Navbar.Brand>
+            <Container>
+            <Navbar expand="lg">
+                <Navbar.Brand href="/"><img src="../images/neardoctor.png" alt="logo" /></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
+                    <Nav className="ml-auto">
                         <Nav.Link href="/conex">Connexion</Nav.Link>
                         <Nav.Link href="/inscri">Inscription</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
+            </Container>
         </header>
     )
 }
