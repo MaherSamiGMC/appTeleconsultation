071e2aee5fa80f2bd407c11f0d1a459ed37da3b6
yosri abdelli <yosriabdelli1@gmail.com>
2021-06-24 18:07:55 +0100

    same main

--- frontend/src/Screens/Dashboard/MainPage.js
+++ frontend/src/Screens/Dashboard/MainPage.js
@@ -2,18 +2,18 @@ import React from 'react'
 import AppSidebar from '../../Components/Dashboard/AppSidebar'
 import AppHeader from '../../Components/Dashboard/AppHeader'
 import AppFooter from '../../Components/Dashboard/AppFooter'
-import {CContainer} from '@coreui/react'
+import {CContainer,CCol } from '@coreui/react'
 
 const MainPage = () => {
   return (
-    <CContainer>
-      <AppSidebar/>
-      <div className="wrapper d-flex flex-column min-vh-100 bg-light">
-        {/* <AppHeader /> */}
-        <div className="body flex-grow-1 px-3">
-        </div>
-        {/* <AppFooter /> */}
-      </div>
+    <CContainer fluid >
+        <CCol sm="auto">
+            <AppSidebar/>
+        </CCol>
+        <CCol sm="auto">
+
+        </CCol>
+ 
     </CContainer>
   )
 }
