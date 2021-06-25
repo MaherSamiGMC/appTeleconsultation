142da930f07b31b88001a33cb708045b63ea9e96
Maher Sami <samimaher.ensit@gmail.com>
2021-06-25 23:22:54 +0100

    frontend auth

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
+import {Provider} from 'react-redux'
+import store from './Redux/Store';
+
 
 ReactDOM.render(
-    <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 
