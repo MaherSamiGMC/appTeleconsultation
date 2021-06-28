e64f3844d82692595d83055d67550fd2914c3632
yosri abdelli <yosriabdelli1@gmail.com>
2021-06-28 05:07:39 +0100

    style

--- frontend/src/Screens/DocPortal/MainDocPortal.js
+++ frontend/src/Screens/DocPortal/MainDocPortal.js
@@ -1,16 +1,18 @@
 import React, {useState, useEffect} from 'react'
-import { Container, Col, Row, Form, Button } from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
 import {useDispatch,useSelector} from 'react-redux'
 import { Getuserdetails } from '../../Redux/Actions/userActions'
 import Message from '../../Components/LandingPage/Message'
 import Loader from '../../Components/LandingPage/Loader'
 
+import Header from '../../Components/LandingPage/Header'
+import Footer from '../../Components/LandingPage/Footer'
 
 const MainDocPortal = ({ history }) => {
 
     const dispatch = useDispatch()
     const userLogin = useSelector(state => state.userLogin)
-    const {userInfo}=userLogin
+    const {userInfo} = userLogin
     const userDetails = useSelector(state => state.userDetails)
     const {Loading,error,user}=userDetails
 
@@ -18,16 +20,21 @@ const MainDocPortal = ({ history }) => {
         if (!userInfo){
             history.push('/connexion')
         } else {
-        dispatch(Getuserdetails('portail'))
+        dispatch(Getuserdetails('Portail'))
         }
     },[dispatch,userInfo,history])
 
     return (
-        <Container>
-            {error && <Message variant='danger'>{error}</Message>}
-            {/* <Portail userdetails={user.getPortail}/> */}
-            Bienvenue
-        </Container>
+        <>  
+            <Header />
+            <Container>
+                {Loader}
+                {error && <Message variant='danger'>{error}</Message>}
+                {/* <Portail userdetails={user.getPortail}/> */}
+                Bienvenue
+            </Container>
+            <Footer />
+        </>
     )
 }
 
